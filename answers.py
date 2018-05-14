c062aff8b652b8dd02639dcf42234d1c8d18b026
Bryan <willimsbw@gmail.com>
2018-05-14 11:59:52 -0400

    Abstracted getting query results to function to avoid repeat code

--- answers.py
+++ answers.py
@@ -4,26 +4,27 @@ from datetime import datetime
 
 #Write three functions, one for each question, and a fourth that calls them in order
 
+def get_results(c, query):
+    c.execute(query)
+    return c.fetchall()
+
 #top_three_articles
 def top_three_articles(c):
-    c.execute("SELECT * FROM top_articles")
-    response = c.fetchall()
+    response = get_results(c, "SELECT * FROM top_articles")
     print "\nThe 3 most viewed articles are:\n"
     for row in response:
         print "  * " + row[0] + " --- " + str(row[1]) + " views."
 
 #authors_in_order
 def authors_in_order(c):
-    c.execute("SELECT * FROM authors_views")
-    response = c.fetchall()
+    response = get_results(c, "SELECT * FROM authors_views")
     print "\n\nHere are all authors on the site, in order of whose articles have gotten the most views over time:\n"
     for row in response:
         print "  * " + row[0] + " --- " + str(row[1]) + " views."
 
 #lots_o_failures
 def high_failure_rate(c):
-    c.execute("SELECT * FROM many_failures")
-    response = c.fetchall()
+    response = get_results(c, "SELECT * FROM many_failures")
     print "\n\nThese are the dates where more than 1{} of requests to the server resulted in an error:\n".format("%")
     for row in response:
         new_format_date = row[1].strftime("%B %d, %Y")
