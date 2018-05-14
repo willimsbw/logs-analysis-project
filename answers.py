736c1bf89adeb41059ef13db337c73f34a31050d
Bryan <willimsbw@gmail.com>
2018-05-14 10:56:56 -0400

    Mapped out program framework

--- answers.py
+++ answers.py
@@ -1,1 +1,27 @@
 #!/usr/bin/env python3
+import psycopg2
+
+#Write three functions, one for each question, and a fourth that calls them in order
+
+#top_three_articles
+def top_three_articles:
+
+
+
+#authors_in_order
+def authors_in_order:
+
+
+#lots_o_failures
+def high_failure_rate:
+
+
+def print_report:
+    #open connection
+    #create_views
+    #top_three_articles
+    #authors_in_order
+    #lots_o_failures
+    #close connection
+
+print_report()
