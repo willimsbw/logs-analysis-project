89ceb7af4cb4c7fc3f9eacacd4a6dd3eddd98204
Bryan <willimsbw@gmail.com>
2018-05-14 10:48:13 -0400

    Wrote query to create view for Question 3.

--- create-views.sql
+++ create-views.sql
@@ -5,7 +5,7 @@ CREATE OR REPLACE VIEW articles_info AS SELECT articles.title, articles.author, 
       WHERE status='200 OK'
       GROUP BY path
   ) AS article_views
-  WHERE article_views.path=CONCAT('/article/', articles.slug)
+  WHERE article_views.path=CONCAT('/article/', articles.slug);
 
 
 CREATE OR REPLACE VIEW top_articles AS SELECT title, visits
@@ -21,44 +21,21 @@ CREATE OR REPLACE VIEW authors_views AS SELECT authors.name, sum(articles_info.v
 
 CREATE OR REPLACE VIEW many_failures AS SELECT *
   FROM (
-    SELECT CAST(CAST(failed_requests.num AS DECIMAL (10,2))/CAST(all_requests.num AS DECIMAL (10,2)) AS DECIMAL (10,2)) AS percent, all_requests.time
-    FROM (
-      SELECT count(*) as num, time::date
-        FROM log
-        WHERE status!='200 OK'
-        GROUP BY time::date
-        ORDER BY num DESC
-    ) AS failed_requests, (
-      SELECT count(*) as num, time::date
-        FROM log
-        GROUP BY time::date
-        ORDER BY num DESC
-    ) AS all_requests
-    WHERE failed_requests.time=all_requests.time
-    ORDER BY percent DESC
-    ) AS percent_failures
-  WHERE percent>0.01;
-
-
-
-
-  select count(*) as num, time::date
-    FROM log
-    WHERE status!='200 OK'
-    GROUP BY time::date
-    ORDER BY num DESC;
-
-
-  SELECT count(*) as num, time::date
-    FROM log
-    GROUP BY time::date
-    ORDER BY num DESC;
-
-SELECT *
-  FROM (
     SELECT CAST(CAST(failed_requests.num AS DECIMAL (10,2))/CAST(all_requests.num AS DECIMAL (10,2)) AS DECIMAL (10,2)) AS percent, all_requests.time
-      FROM failed_requests, all_requests
+      FROM (
+        SELECT count(*) as num, time::date
+          FROM log
+          WHERE status!='200 OK'
+          GROUP BY time::date
+          ORDER BY num DESC
+      ) AS failed_requests,
+      (
+        SELECT count(*) as num, time::date
+          FROM log
+          GROUP BY time::date
+          ORDER BY num DESC
+      ) AS all_requests
       WHERE failed_requests.time=all_requests.time
       ORDER BY percent DESC
-    ) AS foo
-    WHERE percent>0.01;
+  ) AS percent_failures
+  WHERE percent>0.01;
