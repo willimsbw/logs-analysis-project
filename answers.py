a07887af4b029c9946eb1bae958e3090852c991a
Bryan <willimsbw@gmail.com>
2018-05-14 11:06:24 -0400

    Completed top_three_articles function

--- answers.py
+++ answers.py
@@ -6,14 +6,18 @@ import create_views.sql
 
 #top_three_articles
 def top_three_articles(db):
+    c = db.cursor()
+    c.execute("SELECT * FROM top_articles")
+    response = c.fetchall();
+    print "The 3 most viewed articles are:"
+    for row in response:
+        print row[0] + " --- " + row[1] + " views."
 
 #authors_in_order
 def authors_in_order(db):
 
-
 #lots_o_failures
 def high_failure_rate(db):
-
 
 def print_report():
     #open connection
