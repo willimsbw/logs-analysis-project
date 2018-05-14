f63ea0ab8342b2483506e631b12c2b93c184ac3b
Bryan <willimsbw@gmail.com>
2018-05-14 11:00:16 -0400

    completed framework of program

--- answers.py
+++ answers.py
@@ -1,22 +1,21 @@
 #!/usr/bin/env python3
 import psycopg2
+import create_views.sql
 
 #Write three functions, one for each question, and a fourth that calls them in order
 
 #top_three_articles
-def top_three_articles:
-
-
+def top_three_articles(db):
 
 #authors_in_order
-def authors_in_order:
+def authors_in_order(db):
 
 
 #lots_o_failures
-def high_failure_rate:
+def high_failure_rate(db):
 
 
-def print_report:
+def print_report():
     #open connection
     #create_views
     #top_three_articles
