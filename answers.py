8289985b20b5519689bfba59462ad5964a7c57f5
Bryan <willimsbw@gmail.com>
2018-05-14 12:08:27 -0400

    Fixed styling to adhere to pep8

--- answers.py
+++ answers.py
@@ -7,24 +7,31 @@ def get_results(c, query):
     c.execute(query)
     return c.fetchall()
 
+
 def top_three_articles(c):
     response = get_results(c, "SELECT * FROM top_articles")
     print "\nThe 3 most viewed articles are:\n"
     for row in response:
         print "  * " + row[0] + " --- " + str(row[1]) + " views."
 
+
 def authors_in_order(c):
     response = get_results(c, "SELECT * FROM authors_views")
-    print "\n\nHere are all authors on the site, in order of whose articles have gotten the most views over time:\n"
+    print "\n\nHere are all authors on the site, in order of whose articles "
+    + "have gotten the most views over time:\n"
     for row in response:
         print "  * " + row[0] + " --- " + str(row[1]) + " views."
 
+
 def high_failure_rate(c):
     response = get_results(c, "SELECT * FROM many_failures")
-    print "\n\nThese are the dates where more than 1{} of requests to the server resulted in an error:\n".format("%")
+    print "\n\nThese are the dates where more than 1{} of requests to the "
+    + "server resulted in an error:\n".format("%")
     for row in response:
         new_format_date = row[1].strftime("%B %d, %Y")
-        print "  * " + str(new_format_date) + " --- " + str(int(row[0] * 100)) + "{} errors\n".format("%")
+        print "  * " + str(new_format_date) + " --- " + str(int(row[0] * 100))
+        + "{} errors\n".format("%")
+
 
 def print_report(dbname):
     db = psycopg2.connect("dbname=" + dbname)
@@ -35,5 +42,6 @@ def print_report(dbname):
     authors_in_order(c)
     high_failure_rate(c)
     db.close()
+
 
 print_report("news")
