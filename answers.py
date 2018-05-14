1be3322869736998ab6f5cc88b62cf0e144d523d
Bryan <willimsbw@gmail.com>
2018-05-14 13:01:59 -0400

    Added if statements in case no records are returned

--- answers.py
+++ answers.py
@@ -10,27 +10,37 @@ def get_results(c, query):
 
 def top_three_articles(c):
     response = get_results(c, "SELECT * FROM top_articles")
-    print "\nThe 3 most viewed articles are:\n"
-    for row in response:
-        print "  * " + row[0] + " --- " + str(row[1]) + " views."
+    if len(response) > 0:
+        print "\nThe most viewed articles are:\n"
+        for row in response:
+            print "  * " + row[0] + " --- " + str(row[1]) + " views."
+    else:
+        print "\nThere were no articles with view numbers in the database."
 
 
 def authors_in_order(c):
     response = get_results(c, "SELECT * FROM authors_views")
-    print "\n\nHere are all authors on the site, in order of whose articles "
-    + "have gotten the most views over time:\n"
-    for row in response:
-        print "  * " + row[0] + " --- " + str(row[1]) + " views."
+    if len(response) > 0:
+        print "\n\nHere are all authors on the site, in order of whose"
+        + " articles have gotten the most views over time:\n"
+        for row in response:
+            print "  * " + row[0] + " --- " + str(row[1]) + " views."
+    else:
+        print "\nThere were no authors with view numbers in the database."
 
 
 def high_failure_rate(c):
     response = get_results(c, "SELECT * FROM many_failures")
-    print "\n\nThese are the dates where more than 1{} of requests to the "
-    + "server resulted in an error:\n".format("%")
-    for row in response:
-        new_format_date = row[1].strftime("%B %d, %Y")
-        print "  * " + str(new_format_date) + " --- " + str(int(row[0] * 100))
-        + "{} errors\n".format("%")
+    if len(response) > 0:
+        print "\n\nThese are the dates where more than 1{} of requests to the "
+        + "server resulted in an error:\n".format("%")
+        for row in response:
+            new_format_date = row[1].strftime("%B %d, %Y")
+            print "  * " + str(new_format_date) + " --- " + str(int(row[0] * 100))
+             + "{} errors\n".format("%")
+    else:
+        print "\nThere were no days where more than 1% of requests resulted"
+        + " in an error"
 
 
 def print_report(dbname):
