c87a612b1e7b66072f6f5c6b96a4a794fcc550b7
Bryan <willimsbw@gmail.com>
2018-05-14 11:19:21 -0400

    Completed and tested top_three_articles

--- answers.py
+++ answers.py
@@ -1,28 +1,30 @@
 #!/usr/bin/env python3
 import psycopg2
-import create_views.sql
 
 #Write three functions, one for each question, and a fourth that calls them in order
 
 #top_three_articles
-def top_three_articles(db):
-    c = db.cursor()
+def top_three_articles(c):
     c.execute("SELECT * FROM top_articles")
     response = c.fetchall();
     print "The 3 most viewed articles are:"
     for row in response:
-        print row[0] + " --- " + row[1] + " views."
+        print row[0] + " --- " + str(row[1]) + " views."
 
 #authors_in_order
 def authors_in_order(db):
+    #content goes here
 
 #lots_o_failures
 def high_failure_rate(db):
+    #content goes here
 
 def print_report():
-    #open connection
-    #create_views
-    #top_three_articles
+    db = psycopg2.connect("dbname=news")
+    c = db.cursor()
+    query = open("create-views.sql").read()
+    c.execute(query)
+    top_three_articles(c)
     #authors_in_order
     #lots_o_failures
     #close connection
