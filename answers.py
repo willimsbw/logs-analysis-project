fc39d0aa8b5ee99c88cb8cdccb02a80d26b107b7
Bryan <willimsbw@gmail.com>
2018-05-14 11:37:07 -0400

    Added a few more line breaks and fixed % signs and decimal places in high_failure_rate printout

--- answers.py
+++ answers.py
@@ -15,7 +15,7 @@ def top_three_articles(c):
 def authors_in_order(c):
     c.execute("SELECT * FROM authors_views")
     response = c.fetchall()
-    print "\nHere are all authors on the site, in order of whose articles have gotten the most views over time:\n"
+    print "\n\nHere are all authors on the site, in order of whose articles have gotten the most views over time:\n"
     for row in response:
         print row[0] + " --- " + str(row[1]) + " views."
 
@@ -23,9 +23,9 @@ def authors_in_order(c):
 def high_failure_rate(c):
     c.execute("SELECT * FROM many_failures")
     response = c.fetchall()
-    print "\nThese are the dates where more than 1%% of requests to the server results in an error:\n"
+    print "\n\nThese are the dates where more than 1{} of requests to the server results in an error:\n".format("%")
     for row in response:
-        print str(row[1]) + " --- " + str(row[0] * 100) + "%% errors"
+        print str(row[1]) + " --- " + str(int(row[0] * 100)) + "{} errors".format("%")
 
 def print_report():
     db = psycopg2.connect("dbname=news")
